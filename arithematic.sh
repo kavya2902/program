113c9e7f26606cc9941b51f784d4254946a1083d
kavya2902 <kr4273020@gmail.com>
2025-01-06 12:40:11 +0530

    "This program will tell about the airthematic operations in shell scripting."

---
+++ arithematic.sh
@@ -0,0 +1,12 @@
+echo "enter 1st value : "
+read -r a
+echo -e "enter 2nd value : \c"
+read -r b
+
+echo "a + b value is $(($a + $b))"
+echo "a - b value is $(($a - $b))"
+echo "a * b value is $(($a * $b))"
+echo "a / b value is $(($a / $b))"
+echo "a % b value is $(($a % $b))"
+
+
